644eccebb9ba62ca88c71d87b2bb5548109c6251
Khushi Shukla <khushishukla2323@gmail.com>
2025-01-24 11:09:00 +0530

    added db files

---
+++ database/insert_books.sql
@@ -0,0 +1,12 @@
+INSERT INTO books (title, author, published_date) VALUES 
+('To Kill a Mockingbird', 'Harper Lee', '1960-07-11'),
+('1984', 'George Orwell', '1949-06-08'),
+('The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10'),
+('Pride and Prejudice', 'Jane Austen', '1813-01-28'),
+('Alchemist', 'Paulo Coelho', '1851-10-18'),
+('War and Peace', 'Leo Tolstoy', '1869-01-01'),
+('The Catcher in the Rye', 'J.D. Salinger', '1951-07-16'),
+('The Hobbit', 'J.R.R. Tolkien', '1937-09-21'),
+('Brave New World', 'Aldous Huxley', '1932-08-01'),
+('Crime and Punishment', 'Fyodor Dostoevsky', '1866-01-01');
+SELECT * FROM books;
